a7d13c60a6097c2479f0531ea05c817f464c1df9
Justin Greisiger Frost <frostjust@gmail.com>
2022-03-31 15:07:40 -0700

    Update osinfo-db.rb

--- osinfo-db.rb
+++ osinfo-db.rb
@@ -1,8 +1,8 @@
 class OsinfoDb < Formula
   desc "Libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-20191125.tar.xz"
-  sha256 "e8f4e8c3e3ee738191511a4549fc8e4895b501312f2a7d383d5bde72a76f5580"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20210426.tar.xz"
+  sha256 "66c93b4a5b319b2ff7d40360d9e94486a9a45f2c0dd1c30d4b31de4f223070d0"
 
   depends_on "osinfo-db-tools" => :build
 
